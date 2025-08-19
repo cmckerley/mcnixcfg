e8b6841f7870c88537c4e4a770d550b1f893811c
Christopher McKerley <cmckerley@gmail.com>
2025-08-19 15:19:59 -0400

    update vm hw config

--- hosts/nix-vm/hardware-configuration.nix
+++ hosts/nix-vm/hardware-configuration.nix
@@ -9,27 +9,14 @@
   ...
 }: {
   imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod"];
+  boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd"];
+  boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
-  boot.kernelParams = ["amdgpu.securedisplay=0" "preempt=full" "threadirqs"];
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/0a78cb30-c575-4884-ba20-c542212f7acb";
-    fsType = "btrfs";
-    options = ["subvol=@"];
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/ECD0-A62C";
-    fsType = "vfat";
-    options = ["fmask=0022" "dmask=0022"];
-  };
-
-  swapDevices = [];
+  boot.kernelParams = [];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -41,5 +28,4 @@
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
