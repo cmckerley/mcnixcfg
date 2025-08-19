ba6caa00d3934b5b1c6511e97683256b70a65d70
Christopher McKerley <cmckerley@gmail.com>
2025-08-19 15:08:32 -0400

    modified:   hosts/nix-vm/disko-config.nix 	modified:   hosts/nix-vm/system.nix

--- hosts/nix-vm/disko-config.nix
+++ hosts/nix-vm/disko-config.nix
@@ -1,35 +1,26 @@
-# btrfs/disko-config.nix
-
-{ disks ? [ "/dev/vda" ], ... }: 
-let 
-  number_of_disks = if (builtins.length disks < 3) 
-                    then builtins.length disks 
-                    else throw "Error. Too many disks passed to disko.";
-in
 {
-  disko.devices = {
-    disk = {
-      vda = {
-        type = "disk";
-        device = builtins.elemAt disks 0;
+  disko.devices.disk.main = {
+    type = "disk";
+        device = "/dev/vda";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
               # name = "ESP";
-              start = "1MiB";
-              end = "1G";
+              size = "1G";
               type = "EF00";
+              priority = 1;
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
             };
             swap = {
               # name = "swap";
-              start = "1G";
-              end = "9G";
+              size = "8G";
+              priority = 2;
               content = {
                 type = "swap";
                 randomEncryption = true;
@@ -37,14 +28,11 @@ in
             };
             root = {
               # name = "root";
-              start = "9G";
-              end = "100%";
+              size = "100%";
+              priority = 3;
               content = {
                 type = "btrfs";
-                extraArgs = [ "-f" ]; # Override existing partition
-                subvolumes = 
-                  if (number_of_disks == 1) then 
-                    { 
+                subvolumes = {
                       "@" = { };
                       "@/root" = {
                         mountpoint = "/";
@@ -74,71 +62,10 @@ in
                         mountpoint = "/var/tmp";
                         mountOptions = [ "compress=zstd" "noatime" ];
                       };
-                    }
-                  else
-                    {
-                      "@" = { };
-                      "@/root" = {
-                        mountpoint = "/";
-                        mountOptions = [ "compress=zstd" "noatime" ];
-                      };
-                      "@/nix" = {
-                        mountpoint = "/nix";
-                        mountOptions = [ "compress=zstd" "noatime" ];
-                      };
-                      "@/persist" = {
-                        mountpoint = "/persist";
-                        mountOptions = [ "compress=zstd" "noatime" ];
-                      };
-                      "@/var-lib" = {
-                        mountpoint = "/var/lib";
-                        mountOptions = [ "compress=zstd" "noatime" ];
-                      };
-                      "@/var-log" = {
-                        mountpoint = "/var/log";
-                        mountOptions = [ "compress=zstd" "noatime" ];
-                      };
-                      "@/var-tmp" = {
-                        mountpoint = "/var/tmp";
-                        mountOptions = [ "compress=zstd" "noatime" ];
-                      };
                     };
-              };
+                  };
+                };
+             };
             };
-          };
-        };
-      };
-
-      # vdb = if (number_of_disks == 1) then {}
-      # else
-      # {
-      #   type = "disk";
-      #   device = builtins.elemAt disks 1;
-      #   content = {
-      #     type = "gpt";
-      #     partitions = {
-      #       DATA = {
-      #         # name = "DATA";
-      #         start = "1MiB";
-      #         end = "100%";
-      #         content = {
-      #           type = "btrfs";
-      #           extraArgs = [ "-f" ]; # Override existing partition
-      #           subvolumes = {
-      #             "@" = { 
-      #               mountpoint = "/DATA";
-      #               mountOptions = [ "compress=zstd" "noatime" ];
-      #             };
-      #             "@/home" = {
-      #               mountpoint = "/home";
-      #               mountOptions = [ "compress=zstd" ];
-      #             };
-      #           };
-      #         };
-      #       };
-      #     };
-      #   };
-      # };
-    };
-  };
+};
 }
